c2001289ebe1e6e1e435b749f13642601929ff15
sla <uouo33@naver.com>
2019-04-12 21:54:37 +0900

    6 kyu 정답확인 수정필요

--- Codewars/05 - Mexican Wave/wave.js
+++ Codewars/05 - Mexican Wave/wave.js
@@ -5,12 +5,37 @@
 function wave(word){
     const words = word;
     let result = []
+    let upperCase = '';
+    let arrIdx = 0;
 
     for (let i = 0; i < words.length; i++) {
-        result += words;
+        if (words.charAt(i) == ' ') {
+            continue;
+        }
+        upperCase = words.charAt(i).toUpperCase();
+        if (i === 0) {
+            result[0] = `${upperCase}${words.slice(1)}`;
+        } else if (i === words.length-1) {
+            result[arrIdx] = `${words.slice(0,-1)}${upperCase}`;
+        
+        } else if (i !== 0 && i !== words.length - 1){
+            result[arrIdx] = `${words.slice(0, i)}${upperCase}${words.slice(i + 1)}`;
+        }
+        arrIdx += 1;
     }
 
     return result;
 }
 
-console.log(wave("hi"));
+result = ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
+assertDeepEquals(wave("hello"), result);
+
+function assertDeepEquals(ret, correctRet) {
+    for (let index = 0; index < correctRet.length; index++) {
+        if (ret[index] !== correctRet[index]) {
+            return console.log(`${correctRet}`);
+        } else if (index == correctRet.length-1){
+            return console.log(`${correctRet}`);
+        }
+    }
+}
