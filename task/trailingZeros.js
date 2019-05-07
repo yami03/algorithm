58eea89c3df201a163fb7343102d21a45698f495
sla <uouo33@naver.com>
2019-05-07 16:40:11 +0900

    완성 :zap:

--- task/trailingZeros.js
+++ task/trailingZeros.js
@@ -17,20 +17,43 @@
 * @return {number}
 */
 
-// https://onlinemschool.com/math/formula/factorial_table/ 참고
 // 0 - 4 까지 0개       4/5 = 0;
 // 5 - 9 까지 1개       9/5 = 1;
 // 10 - 14 까지 2개    14/5 = 2;
 // 15 - 19 까지 3개    19/5 = 3;
-// 20 - 24 까지 4개    24/5 = 4; 5보다 크면..'ㅁ' 
-// 25 - 29 까지 6개    29/5 = 5로 나눠져서 +1 약수 ..5로 나눴을때..'ㅁ'..; 
-// 30 - 34 까지 7개    34/5 = 6 인데 5로 나눠지니 +1 해서 7개
-// 35 - 39 까지 8개    39/5 = 7인데 5로 나눠지니 +1 해서 8개
-// 40 - 44 까지 9개    44/5 = 8인데 5로 나눠지니 +1 해서 9개
-// 45 - 49 까지 10개   49/5 = 9인데 5로 나눠지니 +1 해서 9개
-// 50 - 54 까지 12까지 54/5 = 10개인데 5로 나눠지는게 +2 12개
-// 100 - 104까지 104/5 = 20 /5   = +4 24개
-// 125 - 129 까지 129/5 = 25 / 5 /5 +6 =31개  
+// 20 - 24 까지 4개    24/5 = 4; 
+// 25 - 29 까지 6개    29/5 = 5 / 5로 한번더 나눌 수 있어서 +1해서 6 
+// 30 - 34 까지 7개    34/5 = 6 +1(5로 나눈 몫) = 7개
+// 35 - 39 까지 8개    39/5 = 7 +1(몫) = 8개
+// 40 - 44 까지 9개    44/5 = 8 +1(몫) = 9개
+// 45 - 49 까지 10개   49/5 = 9 +1(몫) = 10개
+// 50 - 54 까지 12까지 54/5 = 10 +2(몫) = 12개
+// 100 - 104까지       104/5 = 20 +4(몫) = 24개
+// 125 - 129 까지      129/5 = 25+5+1 = 31개
+// 625 - 629 까지      629/5 = 125+25+5+1 = 156개
 function trailingZeros (n) {
-  //5로 나눈다.. 5보다 크면 계속 나눈다.. 5의 약수 구하기.. 랑 비슷..
+  let divisor = 5;
+  let quotient = Math.floor(n / divisor);
+  let result = quotient; 
+  let index = 0
+  
+  //5로만 계속 나눈 후 몫을 구하기
+  for (index; quotient >= divisor; index++) {
+    quotient = Math.floor(quotient / divisor);
+    result += quotient;
+  }
+
+  return result;
+}
+
+assertSimilar(trailingZeros(4), 0);
+assertSimilar(trailingZeros(100), 24);
+assertSimilar(trailingZeros(629), 156);
+assertSimilar(trailingZeros(367419936), 91854977);
+
+function assertSimilar(ret, correctRet) {
+  if (ret === correctRet) {
+    return console.log(`success: ${ret} as expected.`);
+  }
+  console.warn(`failed: ${correctRet}, got ${ret} instead.`);
 }
