22cbed4f89417a7bf344b1a8c6ecadb1b89e21f9
yami03 <seula00027@gmail.com>
2019-09-06 23:05:21 +0900

    6kyu

---
+++ Codewars/48 - Hard Time Bomb/solution.js
@@ -0,0 +1,12 @@
+// link: https://www.codewars.com/kata/52532cc8e9ea83b89b000008
+
+// A bomb has been set to go off!
+// You have to find the wire and cut it in order to stop the timer.
+// There is a global var that holds the numeric ID to which wire to cut.
+// Find that and then you can Bomb.CutTheWire(wireKey);
+
+for (var prop in global) {
+  if (typeof global[prop] === 'number') {
+    global.Bomb.CutTheWire(global[prop]);
+  }
+}
