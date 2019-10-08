a0401037a63ec1d90b8e32a4e02cbb3dcfcc8e4f
yami03 <seula00027@gmail.com>
2019-10-08 18:09:29 +0900

    Lesson 4

---
+++ codility/06 - PermCheck/solution.js
@@ -0,0 +1,59 @@
+// https://app.codility.com/demo/results/trainingRHQA3B-TT6/
+
+// A non-empty array A consisting of N integers is given.
+
+// A permutation is a sequence containing each element from 1 to N once, and only once.
+
+// For example, array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+//     A[3] = 2
+// is a permutation, but array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+// is not a permutation, because value 2 is missing.
+
+// The goal is to check whether array A is a permutation.
+
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+// For example, given array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+//     A[3] = 2
+// the function should return 1.
+
+// Given array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+// the function should return 0.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [1..1,000,000,000].
+
+function solution(A) {
+  const storage = {};
+  for (let i = 1; i <= A.length; i++) {
+    storage[i] = i;
+  }
+
+  for (let j = 0; j < A.length; j++) {
+    delete storage[A[j]];
+  }
+
+  return Object.keys(storage).length ? 0 : 1;
+}
